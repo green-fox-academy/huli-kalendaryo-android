da39c4e342dedf2277d2f9a3d0be7f2ca7cd3423
Magdalena Dzindzisz <dzlica@gmail.com>
2017-12-17 14:40:23 +0100

    retrofit client created

---
+++ app/src/main/java/com/greenfox/kalendaryo/RetrofitClient.java
@@ -0,0 +1,23 @@
+package com.greenfox.kalendaryo;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by lica on 2017. 12. 17..
+ */
+
+public class RetrofitClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
