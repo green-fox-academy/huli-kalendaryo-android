63214826b7bcafb4ab4508a86edc11fe3d7b6806
Dzindzisz Magdalena <dzlica@gmail.com>
2017-12-20 13:46:00 +0100

    style: delete unnecessary lines

--- app/src/main/java/com/greenfox/kalendaryo/RetrofitClient.java
+++ app/src/main/java/com/greenfox/kalendaryo/RetrofitClient.java
@@ -1,9 +1,6 @@
 package com.greenfox.kalendaryo;
 
-import android.util.Log;
-
 import java.util.concurrent.TimeUnit;
-
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
