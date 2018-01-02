f292887604476b77c13f63d0c214e618889c5021
barbarasimandi <barbarasimandi@gmail.com>
2018-01-02 14:49:41 +0100

    deleted CalendarName model, and handling it in CalendarAdapter

--- app/src/main/java/com/greenfox/kalendaryo/httpconnection/ApiService.java
+++ app/src/main/java/com/greenfox/kalendaryo/httpconnection/ApiService.java
@@ -1,14 +1,21 @@
 package com.greenfox.kalendaryo.httpconnection;
 
+import com.google.api.services.calendar.model.Calendar;
 import com.greenfox.kalendaryo.models.KalAuth;
 import com.greenfox.kalendaryo.models.KalUser;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
     @POST("/auth")
     Call<KalUser> getAccessToken(@Body KalAuth auth);
+
+    @GET("/calendars")
+    Call<List<Calendar>> getCalendarList();
 }
