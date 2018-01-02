16246e41235e9009e9cc3b847392a7450ecf73e0
barbarasimandi <barbarasimandi@gmail.com>
2018-01-02 14:14:44 +0100

    CalendarAdapter added, still under progress

---
+++ app/src/main/java/com/greenfox/kalendaryo/models/CalendarName.java
@@ -0,0 +1,25 @@
+package com.greenfox.kalendaryo.models;
+
+/**
+ * Created by barba on 02/01/2018.
+ */
+
+public class CalendarName {
+
+    String calendarName;
+
+    public CalendarName(String calendarName) {
+        this.calendarName = calendarName;
+    }
+
+    public CalendarName() {
+    }
+
+    public String getCalendarName() {
+        return calendarName;
+    }
+
+    public void setCalendarName(String calendarName) {
+        this.calendarName = calendarName;
+    }
+}
