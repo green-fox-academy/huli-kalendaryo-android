5b43c7a70a13be35567cd73635c07b0695f568ee
Dzindzisz Magdalena <dzlica@gmail.com>
2018-01-05 08:28:03 +0100

    feat(constructor) constructor created

--- app/src/main/java/com/greenfox/kalendaryo/models/KalPref.java
+++ app/src/main/java/com/greenfox/kalendaryo/models/KalPref.java
@@ -1,9 +1,26 @@
 package com.greenfox.kalendaryo.models;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lica on 2018. 01. 04..
  */
 
 public class KalPref {
-    
+    private SharedPreferences sharedPref;
+    private List<String> accounts;
+
+
+    public KalPref(Context context) {
+        this.sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+
+    }
+
 }
