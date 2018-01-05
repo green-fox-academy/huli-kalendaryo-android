26a5a1ba7ae281823fd4b785e5a75cefce1ae35b
barbarasimandi <barbarasimandi@gmail.com>
2018-01-05 17:44:48 +0100

    method for getting our accounts to list later set

--- app/src/main/java/com/greenfox/kalendaryo/ChooseAccountActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/ChooseAccountActivity.java
@@ -1,13 +1,38 @@
 package com.greenfox.kalendaryo;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ChooseAccountActivity extends AppCompatActivity {
+import static android.accounts.AccountManager.newChooseAccountIntent;
+
+public class ChooseAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button showAccounts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_account);
+
+        showAccounts = findViewById(R.id.showAccountsForMerging);
+    }
+
+    private void chooseAccount() {
+        // getting the accounts (KalUsers) from KalPref
+        // or not
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.showAccountsForMerging:
+                chooseAccount();
+                break;
+        }
     }
 }
