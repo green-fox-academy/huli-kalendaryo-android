26fbb5a815f00ecb5ded1af37b625bcbf3416847
Marci <szekelymarton00@gmail.com>
2018-01-05 13:26:25 +0100

    added text/listview to activity

--- app/src/main/java/com/greenfox/kalendaryo/SelectCalendarActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/SelectCalendarActivity.java
@@ -2,6 +2,8 @@ package com.greenfox.kalendaryo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SelectCalendarActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SelectCalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_calendar);
+
+        ListView first = findViewById(R.id.firstCalList);
+        ListView second = findViewById(R.id.secondCalList);
+        TextView firstEmail = findViewById(R.id.firstEmail);
+        TextView secondEmail = findViewById(R.id.secondEmail);
+        
     }
+
 }
