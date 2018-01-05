8735ab0611a07ea2883a4d1891d00705adb90968
Marci <szekelymarton00@gmail.com>
2018-01-05 14:37:58 +0100

    added adapter

--- app/src/main/java/com/greenfox/kalendaryo/SelectCalendarActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/SelectCalendarActivity.java
@@ -2,9 +2,15 @@ package com.greenfox.kalendaryo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.greenfox.kalendaryo.models.Kalendar;
 
 public class SelectCalendarActivity extends AppCompatActivity {
 
@@ -13,13 +19,19 @@ public class SelectCalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_calendar);
 
-        ListView listPage = findViewById(R.id.firstCalList);
+        ListView listPage = findViewById(R.id.listPage);
         Button merge = findViewById(R.id.mergeSelectedCalendars);
 
+        listPage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String s = listPage.getItemAtPosition(i).toString();
+
+                Toast.makeText(SelectCalendarActivity.getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
-
-
-
 }
