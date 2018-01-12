be5c375f6cf8e8b3cbe3425e5b5650f9134d4283
barbarasimandi <barbarasimandi@gmail.com>
2018-01-12 11:59:46 +0100

    RadioButton configuration started

--- app/src/main/java/com/greenfox/kalendaryo/adapter/AccountAdapter.java
+++ app/src/main/java/com/greenfox/kalendaryo/adapter/AccountAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.greenfox.kalendaryo.R;
@@ -17,6 +18,7 @@ import com.greenfox.kalendaryo.models.KalUser;
  */
 
 public class AccountAdapter extends ArrayAdapter<String> {
+    int selectedPosition = 0;
 
         public AccountAdapter(@NonNull Context context) {
             super(context, 0);
@@ -28,12 +30,24 @@ public class AccountAdapter extends ArrayAdapter<String> {
 
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.account_name_with_button, parent, false);
+                RadioButton radioButton = convertView.findViewById(R.id.radioButton);
             }
 
             String accountName = getItem(position);
 
             TextView calendarNameView = convertView.findViewById(R.id.accountname);
             calendarNameView.setText(accountName);
+
+            RadioButton radioButton = convertView.findViewById(R.id.radioButton);
+            radioButton.setChecked(position == selectedPosition);
+            radioButton.setTag(position);
+            radioButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedPosition = (Integer)view.getTag();
+                    notifyDataSetChanged();
+                }
+            });
 
             return convertView;
         }
