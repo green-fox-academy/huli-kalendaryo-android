97008eb83d7aee479c2e66507e63749ed37ca56e
barbarasimandi <barbarasimandi@gmail.com>
2018-01-16 18:20:25 +0100

    accountnames in a listview

--- app/src/main/java/com/greenfox/kalendaryo/fragments/AccountsFragment.java
+++ app/src/main/java/com/greenfox/kalendaryo/fragments/AccountsFragment.java
@@ -9,31 +9,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.greenfox.kalendaryo.R;
 import com.greenfox.kalendaryo.adapter.AccountAdapter;
+import com.greenfox.kalendaryo.adapter.AccountsList;
+import com.greenfox.kalendaryo.adapter.KalendarAdapter;
+import com.greenfox.kalendaryo.models.KalAuth;
 import com.greenfox.kalendaryo.models.KalPref;
+
+import java.util.List;
 
 public class AccountsFragment extends Fragment {
 
-    RecyclerView accountNamesView;
+    ListView accountNamesView;
+    AccountsList adapter;
     KalPref kalpref;
-    Button sendToBackend;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_choose_account, container, false);
+
+        View view = inflater.inflate(R.layout.listaccounts, container, false);
         kalpref = new KalPref(getActivity());
-        sendToBackend = view.findViewById(R.id.sendtobackend);
-        accountNamesView = view.findViewById(R.id.accountNames);
-        LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(getActivity());
-        accountNamesView.setLayoutManager(recyclerLayoutManager);
-        DividerItemDecoration dividerItemDecoration =
-                new DividerItemDecoration(accountNamesView.getContext(),
-                        recyclerLayoutManager.getOrientation());
-        accountNamesView.addItemDecoration(dividerItemDecoration);
-        AccountAdapter accountAdapter = new
-                AccountAdapter(kalpref.getKalAuths(),getActivity());
-        accountNamesView.setAdapter(accountAdapter);
+
+        adapter = new AccountsList(view.getContext());
+
+        accountNamesView = view.findViewById(R.id.allaccounts);
+
+        accountNamesView.setAdapter(adapter);
+
+        List<KalAuth> auths = kalpref.getKalAuths();
+
+        adapter.addAll(auths);
+
+
         return view;
     }
 }
