557ec408425091b55d3e726041938cead98c0d31
barbarasimandi <barbarasimandi@gmail.com>
2018-01-17 11:03:16 +0100

    buttons initialized, leaving to different activities

--- app/src/main/java/com/greenfox/kalendaryo/fragments/KalendarFragment.java
+++ app/src/main/java/com/greenfox/kalendaryo/fragments/KalendarFragment.java
@@ -1,15 +1,20 @@
 package com.greenfox.kalendaryo.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.greenfox.kalendaryo.LoginActivity;
 import com.greenfox.kalendaryo.R;
 import com.greenfox.kalendaryo.SelectCalendarActivity;
 import com.greenfox.kalendaryo.adapter.KalendarAdapter;
 import com.greenfox.kalendaryo.httpconnection.ApiService;
 import com.greenfox.kalendaryo.httpconnection.RetrofitClient;
+import com.greenfox.kalendaryo.models.KalPref;
 
 import java.util.ArrayList;
 
@@ -19,10 +24,23 @@ import retrofit2.Response;
 
 public class KalendarFragment extends Fragment {
 
+    KalPref kalpref;
+    FloatingActionButton floatingActionButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.kalendarlist, container, false);
+        floatingActionButton = view.findViewById(R.id.choosecalendar);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), SelectCalendarActivity.class);
+                startActivity(i);
+            }
+        });
 
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+        kalpref = new KalPref(getActivity());
+        return view;
 
     }
 }
