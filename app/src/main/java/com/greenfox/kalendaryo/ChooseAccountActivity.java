ddca0b21c26afa6f2f1a7911ff2ada83aac50ae7
barbarasimandi <barbarasimandi@gmail.com>
2018-01-17 14:39:32 +0100

    finished with design

--- app/src/main/java/com/greenfox/kalendaryo/ChooseAccountActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/ChooseAccountActivity.java
@@ -1,13 +1,16 @@
 package com.greenfox.kalendaryo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 
 import com.greenfox.kalendaryo.adapter.AccountAdapter;
+import com.greenfox.kalendaryo.fragments.KalendarFragment;
 import com.greenfox.kalendaryo.models.KalPref;
 
 
@@ -17,6 +20,7 @@ public class ChooseAccountActivity extends AppCompatActivity {
     KalPref kalpref;
     Button sendToBackend;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,15 @@ public class ChooseAccountActivity extends AppCompatActivity {
         kalpref = new KalPref(this.getApplicationContext());
         sendToBackend = findViewById(R.id.sendtobackend);
 
-        //sendToBackend.setOnClickListener();
+        sendToBackend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(ChooseAccountActivity.this, KalendarFragment.class);
+                startActivity(i);
+            }
+        });
 
 
         accountNamesView = findViewById(R.id.accountNames);
