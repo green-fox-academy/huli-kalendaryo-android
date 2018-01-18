c688ebb1813530425a0f0d0568db9271042bad81
Marci <szekelymarton00@gmail.com>
2018-01-18 14:40:00 +0100

    KalendarAdapter updated

--- app/src/main/java/com/greenfox/kalendaryo/adapter/KalendarAdapter.java
+++ app/src/main/java/com/greenfox/kalendaryo/adapter/KalendarAdapter.java
@@ -1,40 +1,83 @@
 package com.greenfox.kalendaryo.adapter;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+
 import android.widget.TextView;
 
 import com.greenfox.kalendaryo.R;
-import com.greenfox.kalendaryo.models.Kalendar;
+
 
 /**
  * Created by barba on 02/01/2018.
  */
 
-public class KalendarAdapter extends ArrayAdapter<Kalendar> {
+public class KalendarAdapter extends RecyclerView.Adapter<KalendarAdapter.ViewHolder> {
 
-        public KalendarAdapter(@NonNull Context context) {
-            super(context, 0);
-        }
+    private Context context;
+    private ListChange listChange;
 
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-         if (convertView == null) {
-             convertView = LayoutInflater.from(getContext()).inflate(R.layout.calendar_name, parent, false);
-         }
-
-            Kalendar kalendar = getItem(position);
-
-            TextView calendarNameView = convertView.findViewById(R.id.calendarname);
-            calendarNameView.setText(kalendar.getSummary());
-
-            return convertView;
-        }
+    public ListChange getListChange() {
+        return listChange;
     }
+
+    public void setListChange(ListChange listChange) {
+        this.listChange = listChange;
+    }
+
+        public KalendarAdapter(Context context) {
+            this.context = context;
+
+        }
+
+
+    @Override
+    public KalendarAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(KalendarAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView calendarName;
+            private CheckBox checkBox;
+            private boolean checked = true;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            calendarName = itemView.findViewById(R.id.calendarname);
+            checkBox = itemView.findViewById(R.id.checkBox1);
+            checked = checkBox.isChecked();
+
+            switch(itemView.getId()) {
+                case R.id.checkBox1:
+                    if (checked)
+                        listChange.saveCalendar((String)calendarName.getText());
+                    else {
+                        listChange.removeCalendar((String)calendarName.getText());
+                    }
+            }
+
+        }
+
+    }
+    public interface ListChange {
+        void saveCalendar(String calendarTitle);
+        void removeCalendar(String calderTitle);
+    }
+}
+
+
