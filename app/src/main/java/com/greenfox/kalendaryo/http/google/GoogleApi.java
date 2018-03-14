7af43ea9151c507e1059f293f45ac32a53cecb5b
Vajda Lilla <vajdalil.hu@gmail.com>
2018-03-14 17:36:52 +0100

    fix(vajdalil-getEvent) added path variable for getEvent endpoint

--- app/src/main/java/com/greenfox/kalendaryo/http/google/GoogleApi.java
+++ app/src/main/java/com/greenfox/kalendaryo/http/google/GoogleApi.java
@@ -1,12 +1,18 @@
 package com.greenfox.kalendaryo.http.google;
 
+import android.util.EventLog;
+
+import com.google.api.services.calendar.model.Event;
 import com.greenfox.kalendaryo.models.KalendarsResponse;
 import com.greenfox.kalendaryo.models.event.EventsResource;
+
+import java.util.Calendar;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 
 /**
  * Created by BalazsSalfay on 2018. 02. 28..
@@ -20,5 +26,5 @@ public interface GoogleApi {
 
     @Headers("Accept: application/json")
     @GET("{calendarId}/events/{eventId}")
-    Call<EventsResource> getEvents(@Header("Authorization") String authorization);
+    Call<EventsResource> getEvents(@Header("Authorization") String authorization, @Path("calendarId")String calendarId, @Path("eventId")String eventId);
 }
