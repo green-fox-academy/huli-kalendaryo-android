47a72c1462a63fde80b59b2e52d99801756f8e40
Vajda Lilla <vajdalil.hu@gmail.com>
2018-03-18 11:32:03 +0100

    create(vajdalil-getEvent)created an adapter for events, and init a weekview layout

--- app/src/main/java/com/greenfox/kalendaryo/WeekViewActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/WeekViewActivity.java
@@ -6,10 +6,12 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
+import com.greenfox.kalendaryo.adapter.EventAdapter;
 import com.greenfox.kalendaryo.http.RetrofitClient;
 import com.greenfox.kalendaryo.http.google.GoogleApi;
 import com.greenfox.kalendaryo.models.KalAuth;
 import com.greenfox.kalendaryo.models.KalPref;
+import com.greenfox.kalendaryo.models.Kalendar;
 import com.greenfox.kalendaryo.models.event.EventResponse;
 
 import java.io.IOException;
@@ -22,46 +24,12 @@ import retrofit2.Response;
 
 public class WeekViewActivity {
 
-    HttpTransport httpTransport;
-    JsonFactory jsonFactory;
-    Credential credentials;
     private GoogleApi googleApi;
     private KalPref kalPref;
+    private EventAdapter adapter;
+    private Kalendar googleCalendar;
 
     public void getEventList() throws IOException {
-        googleApi = RetrofitClient.getGoogleApi();
-        ArrayList<String> accounts = kalPref.getAccounts();
-
-        googleApi.getEventList(authorization, calendarId).enqueue(new Callback<EventResponse>() {
-            @Override
-            public void onResponse(Call<EventResponse> call, Response<EventResponse> response) {
-
-            }
-
-            @Override
-            public void onFailure(Call<EventResponse> call, Throwable t) {
-
-            }
-        });
-
-        Calendar service = new Calendar
-                .Builder(httpTransport, jsonFactory, credentials)
-                .setApplicationName("Kalendaryo").build();
-
-        String pageToken = null;
-        do {
-            Events events = service.events().list("primary").setPageToken(pageToken).execute();
-            List<Event> items = events.getItems();
-            for (Event event : items) {
-                System.out.println(event.getSummary());
-            }
-            pageToken = events.getNextPageToken();
-        } while (pageToken != null);
-
-    }
-
-
-    public void getCalendarList() {
         googleApi = RetrofitClient.getGoogleApi();
         ArrayList<String> accounts = kalPref.getAccounts();
 
@@ -71,18 +39,20 @@ public class WeekViewActivity {
             String accessToken = kalAuth.getAccessToken();
             String authorization = "Bearer " + accessToken;
 
-            googleApi.getCalendarList(authorization).enqueue(new Callback<KalendarsResponse>() {
+            //todo how to get the googleCalendar of an Auth??? calednarId = WHAT?
+            String calendarId = googleCalendar.getId();
+
+            googleApi.getEventList(authorization, calendarId).enqueue(new Callback<EventResponse>() {
                 @Override
-                public void onResponse(Call<KalendarsResponse> call, Response<KalendarsResponse> response) {
-                    adapter.addKalendars(response.body().getItems());
+                public void onResponse(Call<EventResponse> call, Response<EventResponse> response) {
+                    adapter.addEvents(response.body().getItems());
                 }
 
                 @Override
-                public void onFailure(Call<KalendarsResponse> call, Throwable t) {
-                    t.printStackTrace();
+                public void onFailure(Call<EventResponse> call, Throwable t) {
+
                 }
             });
         }
     }
-
 }
--- app/src/main/java/com/greenfox/kalendaryo/adapter/EventAdapter.java
+++ app/src/main/java/com/greenfox/kalendaryo/adapter/EventAdapter.java
@@ -1,5 +1,67 @@
 package com.greenfox.kalendaryo.adapter;
 
 
+import android.content.Context;
+import android.widget.CompoundButton;
+
+import com.greenfox.kalendaryo.models.event.GoogleEvent;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class EventAdapter {
+
+    private List<GoogleEvent> events;
+    private Context context;
+
+    public void addEvents(List<GoogleEvent> newEvents) {
+        this.events.addAll(newEvents);
+        //notifyDataSetChanged();
+    }
+
+    @Override
+    public void onBindViewHolder(EventAdapter.ViewHolder holder, int position) {
+        GoogleEvent event = events.get(position);
+        holder.calendarName.setText(event.getSummary());
+        holder.checkBox.setOnCheckedChangeListener(null);
+
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listChange.saveCalendar((String)holder.calendarName.getText());
+                else {
+                    listChange.removeCalendar((String)holder.calendarName.getText());
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return kalendars.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView calendarName;
+        private CheckBox checkBox;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            calendarName = itemView.findViewById(R.id.calendarname);
+            checkBox = itemView.findViewById(R.id.checkBox1);
+        }
+
+    }
 }
