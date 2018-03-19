767ba2237777e3fef0397b929dcb99125123e491
Vajda Lilla <vajdalil.hu@gmail.com>
2018-03-19 15:42:24 +0100

    fix(vajdalil-getEvents) created methods and an xml for weekly view

--- app/src/main/java/com/greenfox/kalendaryo/WeekViewActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/WeekViewActivity.java
@@ -1,11 +1,12 @@
 package com.greenfox.kalendaryo;
 
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+
 import com.greenfox.kalendaryo.adapter.EventAdapter;
 import com.greenfox.kalendaryo.http.RetrofitClient;
 import com.greenfox.kalendaryo.http.google.GoogleApi;
@@ -13,21 +14,54 @@ import com.greenfox.kalendaryo.models.KalAuth;
 import com.greenfox.kalendaryo.models.KalPref;
 import com.greenfox.kalendaryo.models.Kalendar;
 import com.greenfox.kalendaryo.models.event.EventResponse;
+import com.greenfox.kalendaryo.models.event.GoogleEvent;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class WeekViewActivity {
+public class WeekViewActivity extends AppCompatActivity {
 
     private GoogleApi googleApi;
     private KalPref kalPref;
     private EventAdapter adapter;
+    private GoogleEvent event;
     private Kalendar googleCalendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weekview);
+
+        adapter = new EventAdapter(this);
+        kalPref = new KalPref(this.getApplicationContext());
+        event = new GoogleEvent();
+        try {
+            getEventList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        recKal = findViewById(R.id.listView);
+        recKal.setAdapter(adapter);
+        LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(this);
+        recKal.setLayoutManager(recyclerLayoutManager);
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recKal.getContext(),
+                        recyclerLayoutManager.getOrientation());
+        recKal.addItemDecoration(dividerItemDecoration);
+        goToChooseAccount = findViewById(R.id.gotochooseaccount);
+        goToChooseAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(WeekViewActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 
     public void getEventList() throws IOException {
         googleApi = RetrofitClient.getGoogleApi();
