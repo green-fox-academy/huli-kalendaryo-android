dd3d450cf052fde9dc54ccba26119a018d1cd0a6
Nóra Varju <snailjokes@gmail.com>
2018-05-25 12:10:47 +0200

    feat(OPAL-162-logout): prevent back button to navigate back after logout

--- app/src/main/java/com/greenfox/kalendaryo/MainActivity.java
+++ app/src/main/java/com/greenfox/kalendaryo/MainActivity.java
@@ -96,7 +96,11 @@ public class MainActivity extends AppCompatActivity {
                                 Log.d("Log out", "User Logged out");
                                 Toast.makeText(getApplicationContext(), "You successfully logged out", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                        Intent.FLAG_ACTIVITY_NEW_TASK);
                                 startActivity(intent);
+                                finish();
                                 }
                             }
                     });
