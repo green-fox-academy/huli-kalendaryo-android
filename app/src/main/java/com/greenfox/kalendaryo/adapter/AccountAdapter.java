8dd0f616dc1dc1168b5cb9bb44819676e10555b8
Isshae <greg.vajtai@gmail.com>
2018-05-31 14:59:48 +0200

    feat(OPAL-228-Account-and-calendar-should-be-selected):first account selected by default

--- app/src/main/java/com/greenfox/kalendaryo/adapter/AccountAdapter.java
+++ app/src/main/java/com/greenfox/kalendaryo/adapter/AccountAdapter.java
@@ -8,26 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.greenfox.kalendaryo.R;
 import com.greenfox.kalendaryo.components.DaggerApiComponent;
 import com.greenfox.kalendaryo.http.backend.BackendApi;
 import com.greenfox.kalendaryo.models.GoogleAuth;
 import com.greenfox.kalendaryo.models.KalPref;
+
 import java.io.IOException;
 import java.util.List;
+
 import javax.inject.Inject;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
 import java.util.ArrayList;
 
 public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.ViewHolder> {
 
     private List<GoogleAuth> auths = new ArrayList<>();
     private Context context;
-    private int lastSelectedPosition = -1;
+    private int lastSelectedPosition = 0;
     private EmailChange emailChange;
     private KalPref kalPref;
     private boolean clickable;
@@ -84,21 +90,21 @@ public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.ViewHold
                             @Override
                             public void onResponse(Call<Void> call, Response<Void> response) {
 
-                                    if (response.errorBody() == null) {
-                                        removeAccount(position);
-                                        Toast.makeText(view.getContext(), "Kalendar deleted successfully", Toast.LENGTH_LONG).show();
-                                    } else {
-                                        try {
-                                            Toast.makeText(view.getContext(), response.errorBody().string() , Toast.LENGTH_LONG).show();
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                        }
+                                if (response.errorBody() == null) {
+                                    removeAccount(position);
+                                    Toast.makeText(view.getContext(), "Kalendar deleted successfully", Toast.LENGTH_LONG).show();
+                                } else {
+                                    try {
+                                        Toast.makeText(view.getContext(), response.errorBody().string(), Toast.LENGTH_LONG).show();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
                                 }
                             }
 
                             @Override
                             public void onFailure(Call<Void> call, Throwable t) {
-                                Toast.makeText(view.getContext(),"Account can not be deleted", Toast.LENGTH_LONG).show();
+                                Toast.makeText(view.getContext(), "Account can not be deleted", Toast.LENGTH_LONG).show();
                             }
                         });
                         dialog.dismiss();
@@ -141,14 +147,13 @@ public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.ViewHold
             if (clickable) {
                 accountName = view.findViewById(R.id.accountname);
                 radioButton = (RadioButton) view.findViewById(R.id.radioButton);
+
                 radioButton.setOnClickListener(v -> {
                     lastSelectedPosition = getAdapterPosition();
                     notifyDataSetChanged();
-
                     if (emailChange != null) {
                         emailChange.emailChanged((String) accountName.getText());
                     }
-
                     Toast.makeText(AccountAdapter.this.context, accountName.getText(),
                             Toast.LENGTH_LONG).show();
                 });
@@ -156,6 +161,8 @@ public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.ViewHold
                 oneAccountName = view.findViewById(R.id.oneAccountName);
             }
         }
+
+
     }
 
     public interface EmailChange {
